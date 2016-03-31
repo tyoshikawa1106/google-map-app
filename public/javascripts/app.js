bcdf51cc489d11312b0975144a951cf400aaa666
Taiki Yoshikawa <tyoshikawa1106@gmail.com>
2016-03-31 14:07:05 +0900

    commit

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -1,59 +1,59 @@
 angular.module("SampleApp", ["Controllers", "ngRoute", "Directives"]).config(["$routeProvider", function($routeProvider){
   $routeProvider.when("/", {
     controller : "HomeController",
-    templateUrl : "/partials/home.html"
+    templateUrl : "/angular_views/home.html"
   }).
   when("/marker", {
     controller : "MarkerController",
-    templateUrl : "/partials/marker.html"
+    templateUrl : "/angular_views/marker.html"
   }).
   when("/marker_multiple", {
     controller : "MarkerMultipleController",
-    templateUrl : "/partials/marker_multiple.html"
+    templateUrl : "/angular_views/marker_multiple.html"
   }).
   when("/info_window", {
     controller : "InfoWindowController",
-    templateUrl : "/partials/info_window.html"
+    templateUrl : "/angular_views/info_window.html"
   }).
   when("/info_window_in_form", {
     controller : "InfoWindowInFormController",
-    templateUrl : "/partials/info_window_in_form.html"
+    templateUrl : "/angular_views/info_window_in_form.html"
   }).
   when("/zoom_event", {
     controller : "ZoomEventController",
-    templateUrl : "/partials/zoom_event.html"
+    templateUrl : "/angular_views/zoom_event.html"
   }).
   when("/map_event", {
     controller : "MapEventController",
-    templateUrl : "/partials/map_event.html"
+    templateUrl : "/angular_views/map_event.html"
   }).
   when("/mvc_object", {
     controller : "MVCObjectController",
-    templateUrl : "/partials/mvc_object.html"
+    templateUrl : "/angular_views/mvc_object.html"
   }).
   when("/polyline", {
     controller : "PolylineController",
-    templateUrl : "/partials/polyline.html"
+    templateUrl : "/angular_views/polyline.html"
   }).
   when("/geocode", {
     controller : "GeocodeController",
-    templateUrl : "/partials/geocode.html"
+    templateUrl : "/angular_views/geocode.html"
   }).
   when("/route_search", {
     controller : "RouteSearchController",
-    templateUrl : "/partials/root_search.html"
+    templateUrl : "/angular_views/root_search.html"
   }).
   when("/elevation", {
     controller : "ElevationController",
-    templateUrl : "/partials/elevation.html"
+    templateUrl : "/angular_views/elevation.html"
   }).
   when("/street_view", {
     controller : "StreetViewController",
-    templateUrl : "/partials/street_view.html"
+    templateUrl : "/angular_views/street_view.html"
   }).
   when("/auto_complete", {
     controller : "AutoCompleteController",
-    templateUrl : "/partials/auto_complete.html"
+    templateUrl : "/angular_views/auto_complete.html"
   }).
   otherwise({
     redirectTo: "/"
